bc9cec5187924572537d376ca19c1f6befb4056c
Martun Salnazaryan <martunsalnazaryan04@gmail.com>
2026-08-16 18:44:41 +0400

    Switch to sqlite package without native compilation errors

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 const { Sequelize } = require('sequelize');
 const path = require('path');
+const sqlite3 = require('sqlite3'); // Այս պակետը կարող է պահանջվել որպես driver
 
-// SQLite-ի դեպքում բազան կպահպանվի պրոեկտի ներսում գտնվող database.sqlite ֆայլում
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: path.join(__dirname, '../database.sqlite'),
+    dialectModule: sqlite3, // Հստակ նշում ենք driver-ը
     logging: false
 });
 
