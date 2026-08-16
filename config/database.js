72552e684804f72c90f6652f38865b735046cc55
Martun Salnazaryan <martunsalnazaryan04@gmail.com>
2026-08-16 18:50:56 +0400

    Add sqlite3 successfully

--- config/database.js
+++ config/database.js
@@ -1,11 +1,9 @@
 const { Sequelize } = require('sequelize');
 const path = require('path');
-const sqlite3 = require('sqlite3'); // Այս պակետը կարող է պահանջվել որպես driver
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: path.join(__dirname, '../database.sqlite'),
-    dialectModule: sqlite3, // Հստակ նշում ենք driver-ը
     logging: false
 });
 
